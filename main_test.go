a4ac40feb467e0238805b9023638b3eb3786a477
Ainur Muratov <141512722+Aiinnur@users.noreply.github.com>
2024-01-07 21:24:38 +0300

    Add TestUnsupportedCity

--- main_test.go
+++ main_test.go
@@ -10,16 +10,18 @@ import (
 )
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	req := httptest.NewRequest(http.MethodGet, "/сafe?count=10&city=moscow", nil) // здесь нужно создать запрос к сервису
+	// здесь нужно создать запрос к сервису
+	req := httptest.NewRequest(http.MethodGet, "/сafe?count=10&city=moscow", nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
 	// здесь нужно добавить необходимые проверки
-	require.Equal(t, http.StatusOK, responseRecorder.Code, "Expected status code %d, but got %d", http.StatusOK, responseRecorder.Code)
+	require.Equal(t, http.StatusOK, responseRecorder.Code, "Expected status code %d, but got %d",
+		http.StatusOK, responseRecorder.Code)
 	expectedBody := "Мир кофе,Сладкоежка,Кофе и завтраки,Сытый студент"
-	assert.Equal(t, expectedBody, responseRecorder.Body.String(), "Expected body %d, but got %d",
+	assert.Equal(t, expectedBody, responseRecorder.Body.String(), "Expected body %s, but got %s",
 		expectedBody, responseRecorder.Body.String())
 }
 
@@ -31,6 +33,21 @@ func TestMainHandlerValidRequest(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	assert.Equal(t, http.StatusOK, responseRecorder.Code, "Expected status code %d, but got %d", http.StatusOK, responseRecorder.Code)
+	assert.Equal(t, http.StatusOK, responseRecorder.Code, "Expected status code %d, but got %d",
+		http.StatusOK, responseRecorder.Code)
 	assert.NotEmpty(t, responseRecorder.Body.String(), "The response body is empty")
+}
+
+func TestUnsupportedCity(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/сafe?count=3&city=Ufa", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code, "Expected status code %d, but got %d",
+		http.StatusBadRequest, responseRecorder.Code)
+	expectedBody := "wrong city value"
+	assert.Equal(t, responseRecorder.Body.String(), "wrong city value", "Expected body %s, but got %s",
+		expectedBody, responseRecorder.Body.String())
 }
