cc58f1c727d2e5c33f4cc9fefe82dbe30f54c049
Ainur Muratov <141512722+Aiinnur@users.noreply.github.com>
2024-01-07 21:16:22 +0300

    Add TestMainHandlerValidRequest function

--- main_test.go
+++ main_test.go
@@ -22,3 +22,15 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	assert.Equal(t, expectedBody, responseRecorder.Body.String(), "Expected body %d, but got %d",
 		expectedBody, responseRecorder.Body.String())
 }
+
+func TestMainHandlerValidRequest(t *testing.T) {
+	req, err := http.NewRequest("GET", "/cafe?count=3&city=moscow", nil)
+	require.NoError(t, err, "The request is not formed correctly")
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	assert.Equal(t, http.StatusOK, responseRecorder.Code, "Expected status code %d, but got %d", http.StatusOK, responseRecorder.Code)
+	assert.NotEmpty(t, responseRecorder.Body.String(), "The response body is empty")
+}
